f6cabfa7cef25969c06dc2587f8db6f1c5f5e737
Snak <snak.hoshinonaka@gmail.com>
2025-03-01 09:50:43 +0000

    ページのタイトルを修正

--- spec/requests/static_pages_controller_spec.rb
+++ spec/requests/static_pages_controller_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe "StaticPages", type: :request do
-  describe "GET /home" do
+RSpec.describe StaticPagesController, type: :request do
+  describe "GET #home" do
     it "returns http success" do
       get root_path
       expect(response).to have_http_status(:success)
