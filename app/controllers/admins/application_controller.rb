ea8caf3d7d96392dd89acf8f1b2b11560aa8f13a
Snak <72hoshino.snak@gmail.com>
2025-03-10 09:06:25 +0000

    authenticationの継承を修正する

--- app/controllers/admins/application_controller.rb
+++ app/controllers/admins/application_controller.rb
@@ -1,3 +1,2 @@
 class Admins::ApplicationController < ApplicationController
-  include Authentication
 end
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::Base
   include Authentication
-  allow_unauthenticated_access
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern unless ENV.fetch("RAILS_ENV", "development") == "development"
 end
